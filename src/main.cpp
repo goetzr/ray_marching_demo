0c71b1a15dddf8a573e2ee736397dd0173c3c92d
Russ Goetz <russgoetz@gmail.com>
2026-01-02 20:04:58 -0500

    Integrated the CMake Tools extension so that intellisense now works properly for SDL3

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include "pch.h"
 
+// #define SDL_MAIN_USE_CALLBACKS 1  /* use the callbacks instead of main() */
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+
 int main(int argc, char** argv) {
     std::cout << "Ray marching demo" << "\n";
     return 0;
