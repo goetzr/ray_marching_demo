9bdd9f91810426953e28c0ca967858bbb2995079
Russ Goetz <russgoetz@gmail.com>
2026-03-02 19:35:49 -0500

    Removed logging statements I forgot about.

--- src/camera.cpp
+++ src/camera.cpp
@@ -12,20 +12,9 @@ Camera::Camera(double sensor_aspect_ratio, double fov_horiz, SensorFit sensor_fi
     fov_.horiz = deg2rad(fov_horiz);
     fov_.vert = deg2rad(fov_horiz / sensor_aspect_ratio);
 
-    #ifdef LOG
-    std::cout << "FOV horizontal = " << rad2deg(fov_.horiz) << " deg\n";
-    std::cout << "FOV vertical = " << rad2deg(fov_.vert) << "  deg\n";
-    #endif
-
     rot_x = deg2rad(rot_x);
     rot_y = deg2rad(rot_y);
     rot_z = deg2rad(rot_z);
-
-    #ifdef LOG
-    std::cout << "x-axis rotation = " << rad2deg(rot_x) << " deg\n";
-    std::cout << "y-axis rotation = " << rad2deg(rot_y) << " deg\n";
-    std::cout << "z-axis rotation = " << rad2deg(rot_z) << " deg\n";
-    #endif
 
     // The camera starts with the same basis vectors as the world coordinate system.
     Mat3 basis {
@@ -60,12 +49,6 @@ Camera::Camera(double sensor_aspect_ratio, double fov_horiz, SensorFit sensor_fi
     j_hat_ = new_basis.c[1];
     k_hat_ = new_basis.c[2];
 
-    #ifdef LOG
-    std::cout << "i_hat = { " << i_hat_.x << ", " << i_hat_.y << ", " << i_hat_.z << " }\n";
-    std::cout << "j_hat = { " << j_hat_.x << ", " << j_hat_.y << ", " << j_hat_.z << " }\n";
-    std::cout << "k_hat = { " << k_hat_.x << ", " << k_hat_.y << ", " << k_hat_.z << " }\n";
-    #endif
-
     // NOTE: This is unnecessary for a raymarcher. The desired FOV is specified directly.
     // fov_.horiz = 2 * std::atan2(sensor_sz_.width  * kInchesToMm / 2,  focal_len_);
     // fov_.vert  = 2 * std::atan2(sensor_sz_.height * kInchesToMm / 2, focal_len_);
@@ -85,23 +68,4 @@ void Camera::calc_world_to_camera() noexcept {
 
     // Invert the translation (position) using the inverted rotation matrix.
     world_to_camera_.translation = -(world_to_camera_.rotation * pos_);
-
-    #ifdef LOG
-    std::cout << "World to camera, i_hat = { "
-        << world_to_camera_.rotation.c[0].x << ", "
-        << world_to_camera_.rotation.c[0].y << ", "
-        << world_to_camera_.rotation.c[0].z << " }\n";
-    std::cout << "World to camera, j_hat = { "
-        << world_to_camera_.rotation.c[1].x << ", "
-        << world_to_camera_.rotation.c[1].y << ", "
-        << world_to_camera_.rotation.c[1].z << " }\n";
-    std::cout << "World to camera, k_hat = { "
-        << world_to_camera_.rotation.c[2].x << ", "
-        << world_to_camera_.rotation.c[2].y << ", "
-        << world_to_camera_.rotation.c[2].z << " }\n";
-    std::cout << "World to camera, translation = { "
-        << world_to_camera_.translation.x << ", "
-        << world_to_camera_.translation.y << ", "
-        << world_to_camera_.translation.z << " }\n";
-    #endif
 }
